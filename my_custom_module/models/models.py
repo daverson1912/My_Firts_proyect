955c9732d7a45265b42d46a0a80dbb3ecff93b81
daverson1912 <daverson1912@users.noreply.github.com>
2026-06-17 15:56:07 -0400

    Esto es una prueba

--- my_custom_module/models/models.py
+++ my_custom_module/models/models.py
@@ -8,3 +8,4 @@ class MyCustomModel(models.Model):
     name = fields.Char(string='Nombre', required=True)
     description = fields.Text(string='Descripción')
     active = fields.Boolean(string='Activo', default=True)
+    comentario = fields.Char(string='Comentario de Prueba')
